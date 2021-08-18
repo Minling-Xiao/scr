deb6b41313e99f62509eec93fa7fa2cdf31e7334
Minling-Xiao <87986393+Minling-Xiao@users.noreply.github.com>
2021-08-19 01:24:45 +1200

    Add files via upload

---
+++ interpolation-idw.R
@@ -0,0 +1,69 @@
+library(gstat)
+
+#covariate df
+load("test-data.RData")
+mask.dt = as.data.frame(test.data$mask)
+mask.dt = mask.dt/100
+
+n1 = 1e5L
+first.covariate.dt = data.frame(
+  X = sort(rnorm(n1)), Y = rnorm(n1),
+  var1 = rpois(n1, lambda = pi), var2 = sort(runif(n1)))
+
+n2 = 1e3L
+index = sample(1L:n1, n2)
+second.covariate.dt = data.frame(
+  X = c(first.covariate.dt$X[index], rnorm(n2)),
+  Y = sort(c(first.covariate.dt$Y[index], rnorm(n2))),
+  var2 = c(first.covariate.dt$var2[index], runif(n2)),
+  var3 = sample(x = LETTERS, size = n2*2L, replace = TRUE),
+  var4 = sort(rnorm(n2*2L)))
+
+n3 = 1e2L
+third.covariate.dt = data.frame(
+  X = rnorm(n3), Y = rnorm(n3))
+
+cov.bind = function(cov.var, ...) {
+  cov.list = list(...)
+  names(cov.list) = 1:length(cov.list)
+  col = c("X", "Y", cov.var)
+  
+  df = data.frame()
+  for (i in 1:length(cov.list)) {
+    cov.df = cov.list[[i]]
+    if (cov.var %in% names(cov.df)) {
+      df = rbind(df, cov.df[col])
+    }
+  }
+  df
+}
+
+plot = function(mask, var, formula, ...){
+  df = mask
+  coordinates(df) = ~ x + y
+  gridded(df) = TRUE
+  
+  var.df = cov.bind(var,
+                    first.covariate.dt,
+                    second.covariate.dt,
+                    third.covariate.dt)
+  coordinates(var.df) = ~ X + Y
+  
+  # Inverse Distance Weighting
+  idw = idw(formula = formula,
+             locations = var.df,
+             newdata = df)
+  idw.output = as.data.frame(idw)
+  names(idw.output)[1:3] = c("x", "y", "prediction")
+  
+  ggplot(data = idw.output, aes(x = x, y = y)) +
+    geom_tile(data = idw.output, aes(fill = prediction)) +
+    scale_fill_gradient(low = "#FEEBE2", high = "#7A0177") +
+    coord_equal()
+}
+
+plot(mask.dt,"var2",var2 ~ 1,first.covariate.dt,second.covariate.dt,third.covariate.dt)
+
+
+
+
