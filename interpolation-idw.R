d0f9edea922889b9c2d7c29e7d975818e676def7
Minling-Xiao <87986393+Minling-Xiao@users.noreply.github.com>
2021-08-25 02:01:59 +1200

    Update interpolation-idw.R

--- interpolation-idw.R
+++ interpolation-idw.R
@@ -1,4 +1,6 @@
 library(gstat)
+library(sp)
+library(ggplot2)
 
 #covariate df
 load("test-data.RData")
@@ -14,7 +16,7 @@ n2 = 1e3L
 index = sample(1L:n1, n2)
 second.covariate.dt = data.frame(
   X = c(first.covariate.dt$X[index], rnorm(n2)),
-  Y = sort(c(first.covariate.dt$Y[index], rnorm(n2))),
+  Y = c(first.covariate.dt$Y[index], rnorm(n2)),
   var2 = c(first.covariate.dt$var2[index], runif(n2)),
   var3 = sample(x = LETTERS, size = n2*2L, replace = TRUE),
   var4 = sort(rnorm(n2*2L)))
@@ -23,6 +25,7 @@ n3 = 1e2L
 third.covariate.dt = data.frame(
   X = rnorm(n3), Y = rnorm(n3))
 
+###
 cov.bind = function(cov.var, ...) {
   cov.list = list(...)
   names(cov.list) = 1:length(cov.list)
@@ -38,32 +41,51 @@ cov.bind = function(cov.var, ...) {
   df
 }
 
-plot = function(mask, var, formula, ...){
+###
+plot = function(mask, var, formula, nmax=1, maxdist=1, ...){
   df = mask
   coordinates(df) = ~ x + y
   gridded(df) = TRUE
   
-  var.df = cov.bind(var,
-                    first.covariate.dt,
-                    second.covariate.dt,
-                    third.covariate.dt)
-  coordinates(var.df) = ~ X + Y
+  output.df = matrix(NA, nrow = nrow(mask), ncol = length(var))
+  output.df = data.frame(mask,output.df)
+  plot.list = list()
   
-  # Inverse Distance Weighting
-  idw = idw(formula = formula,
-             locations = var.df,
-             newdata = df)
-  idw.output = as.data.frame(idw)
-  names(idw.output)[1:3] = c("x", "y", "prediction")
+  for (i in 1:length(var)) {
+    var.df = cov.bind(var[i],...)
+    coordinates(var.df) = ~ X + Y
+    
+    # Inverse Distance Weighting
+    idw = idw(
+      formula = as.formula(formula[i]),
+      locations = var.df,
+      newdata = df,
+      nmax = nmax,
+      maxdist = maxdist
+    )
+    idw.output = as.data.frame(idw)
+    names(idw.output)[1:3] = c("x","y","Prediction")
+    output.df[i+2] = idw.output[3]
+    
+    plot.list[[i]] = ggplot(data = idw.output, aes(x = x, y = y)) +
+      geom_tile(data = idw.output, aes(fill = Prediction)) +
+      scale_fill_gradient(low = "#FEEBE2", high = "#7A0177") +
+      coord_equal()+
+      ggtitle(formula[i])
+  }
   
-  ggplot(data = idw.output, aes(x = x, y = y)) +
-    geom_tile(data = idw.output, aes(fill = prediction)) +
-    scale_fill_gradient(low = "#FEEBE2", high = "#7A0177") +
-    coord_equal()
+  names(output.df)=c("x", "y", formula)
+  names(plot.list) = formula
+  multi.return = list(output.df, plot.list)
+  
+  return(multi.return)
 }
 
-plot(mask.dt,"var2",var2 ~ 1,first.covariate.dt,second.covariate.dt,third.covariate.dt)
-
+plot(mask = mask.dt, 
+     var = c("var1","var2","var4"),
+     formula = c("var1 ~ 1","var2 ~ 1","var4 ~ 1"), 
+     nmax = 5, maxdist = 5,
+     first.covariate.dt,second.covariate.dt,third.covariate.dt)
 
 
 
